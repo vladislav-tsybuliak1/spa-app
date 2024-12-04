4eeef3b435a2c29ed77a705aca9d6e0da3ffc63b
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-04 19:40:45 +0200

    Add username validator to models & migration file

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from user.validators import validate_username
+
 
 class User(AbstractUser):
-    username = models.CharField(max_length=31, unique=True)
+    username = models.CharField(
+        max_length=31,
+        unique=True,
+        validators=[validate_username],
+    )
     email = models.EmailField(unique=True)
