aab970b20f7b66760075b36de59d1ab5579f5ab0
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-04 22:50:35 +0200

    Add token obtain, refresh & verify endpoints

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 from user.views import CreateUserView
 
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create-user"),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
 ]
 
 app_name = "user"
