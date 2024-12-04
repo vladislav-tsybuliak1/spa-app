76c569f49fd89e3fdf000a442a639e9ccd4811a3
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-04 21:41:35 +0200

    Add media urls

--- backend/spa_service/urls.py
+++ backend/spa_service/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -28,4 +29,4 @@ urlpatterns = [
         "api/v1/comments/",
         include("comment.urls", namespace="comment-api")
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
