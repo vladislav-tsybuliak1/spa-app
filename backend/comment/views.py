bfbc6bdd6cff317632f8f2d90411df4e1b706431
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-04 20:52:54 +0200

    Add comment view

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from comment.models import Comment
+from comment.serializers import CommentSerializer
+
+
+class CommentView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
