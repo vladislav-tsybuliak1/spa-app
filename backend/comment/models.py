37a4740b03b7148913d7d6f306a91319586a770a
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-04 21:42:25 +0200

    Add image field to comment model

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,8 +1,8 @@
-from tkinter.constants import CASCADE
-
 from django.conf import settings
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
+from comment.utils import comment_image_file_path
 from comment.validators import validate_comment_text
 
 
@@ -10,15 +10,26 @@ class Comment(models.Model):
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="comments"
+        related_name="comments",
     )
-    home_page = models.URLField(
-        blank=True,
-        null=True
-    )
+    home_page = models.URLField(blank=True, null=True)
     text = models.TextField(validators=[validate_comment_text])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    attached_image = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to=comment_image_file_path,
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=[
+                    "jpg",
+                    "gif",
+                    "png",
+                ]
+            )
+        ],
+    )
     parent = models.ForeignKey(
         to="self",
         blank=True,
