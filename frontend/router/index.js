bb97e3614ee0827780c3ca2746b891d19e09b055
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-05 00:44:41 +0200

    Add basic router

---
+++ frontend/router/index.js
@@ -0,0 +1,12 @@
+import {createRouter} from 'vue-router';
+
+const routes = [
+    // add here later the routes
+];
+
+const router = createRouter({
+  mode: 'history',
+  routes,
+});
+
+export default router;
