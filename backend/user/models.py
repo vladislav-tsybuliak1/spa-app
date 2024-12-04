436b69e5acd0bb32e2739fcc5364f6d8beb43f35
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-04 19:04:36 +0200

    Rewrite user model with email

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=31, unique=True)
+    email = models.EmailField(unique=True)
