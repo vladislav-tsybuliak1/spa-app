86bee89e049e51365bff8723dd37b0344db58796
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-04 20:33:19 +0200

    Add comment model

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,3 +1,31 @@
+from tkinter.constants import CASCADE
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from comment.validators import validate_comment_text
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    home_page = models.URLField(
+        blank=True,
+        null=True
+    )
+    text = models.TextField(validators=[validate_comment_text])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    reply = models.ForeignKey(
+        to="self",
+        blank=True,
+        null=True,
+        on_delete=CASCADE,
+        related_name="replies",
+    )
+
+    class Meta:
+        ordering = ("-created_at",)
