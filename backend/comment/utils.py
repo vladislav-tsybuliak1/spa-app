e02443ab0239fe1aa90bd96439d54b2de5e737f8
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-04 22:01:56 +0200

    Fix comment images namings

--- backend/comment/utils.py
+++ backend/comment/utils.py
@@ -4,11 +4,9 @@ import uuid
 from django.utils.text import slugify
 
 
-def comment_image_file_path(crew, filename) -> str:
+def comment_image_file_path(comment, filename) -> str:
     _, extension = os.path.splitext(filename)
     filename = (
-        f"{slugify(crew.first_name)}"
-        f"-{slugify(crew.last_name)}"
-        f"-{uuid.uuid4()}{extension}"
+        f"{slugify(comment.created_at)}-{uuid.uuid4()}{extension}"
     )
     return os.path.join("uploads/comments/images/", filename)
