06a860a0957f2aee4b8adea754f2261f596d9479
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-04 20:52:49 +0200

    Add comment serializer

---
+++ backend/comment/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from comment.models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = [
+            "id",
+            "user",
+            "text",
+            "created_at",
+            "updated_at",
+            "reply",
+        ]
