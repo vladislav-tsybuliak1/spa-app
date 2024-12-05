743c315ba5138bb6cad5ef5a2a2ff1797810a5b8
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-05 12:16:28 +0200

    Add comment page to router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,11 +1,13 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import RegistrationPage from './components/RegistrationPage.vue';
 import LogInPage from "./components/LogInPage.vue";
+import CommentsPage from "./components/CommentsPage.vue";
 
 const routes = [
     {path: '/register/', component: RegistrationPage},
     {path: '/', redirect: '/register/'},
     {path: '/login/', component: LogInPage},
+    {path: '/comments/', component: CommentsPage}
 ];
 
 const router = createRouter({
