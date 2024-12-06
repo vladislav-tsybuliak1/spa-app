484e9cb9e126bc4fcd0f3dcb6d90a82ad1d209f3
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-06 16:11:23 +0200

    Change vite settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     vue(),
     vueDevTools(),
@@ -15,4 +16,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  preview: {
+  port: 8080,
+  strictPort: true,
+ },
+  server: {
+  port: 8080,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:8080",
+ },
 })
